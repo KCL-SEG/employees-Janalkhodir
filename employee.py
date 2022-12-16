557f57c57e277fd4dc96e93eab4558304bb8b7a8
Alkhodir, Jana (k20061055) <114947042+Janalkhodir@users.noreply.github.com>
2022-12-16 16:12:46 +0000

    Add Salary Type Classes

--- employee.py
+++ employee.py
@@ -1,15 +1,77 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-class Employee:
-    def __init__(self, name):
-        self.name = name
+class HourlyContract:
+
+    def __init__(self, hourly_pay, hours):
+        self.hourly_pay = hourly_pay
+        self.hours = hours
 
     def get_pay(self):
-        pass
+        return self.hourly_pay * self.hours
 
     def __str__(self):
-        return self.name
+        string = f'contract of {self.hours} hours at {self.hourly_pay} per hpur'
+        return string
+
+class SalaryContract:
+
+    def __init__(self, salary):
+        self.salary = salary
+
+    def get_pay(self):
+        return self.salary
+
+    def __str__(self):
+        string = f' monthly salary of {self.salary}'
+        return string
+
+class BonusCommission:
+
+    def __init__(self, bonus):
+        self.bonus = bonus
+
+    def get_pay(self):
+        return self.bonus 
+
+    def __str__(self):
+        string = f'bonus commission of {self.bonus}'
+        return string
+
+class ContractCommission:
+
+    def __init__(self, contracts=0, commission=200):
+        self.contracts = contracts
+        self.commission = commission
+
+    def get_pay(self):
+        return self.contracts * self.commission
+
+
+    def __str__(self):
+        string = f'Commissin for {self.contracts} contract at {self.commission} commission'
+        return string
+
+
+
+class Employee:
+    def __init__(self, name, contract, commission=None):
+        self.name = name
+        self.contract = contract
+        self.commission = commission
+
+    def get_pay(self):
+        pay = self.contract.get_pay()
+        if self.commission:
+            pay += self.commission.get_pay()
+        return pay
+
+    def __str__(self):
+        string = f'{self.name} works on {str(self.contract)}'
+        if self.commission:
+            string += f'and get {str(self.commission)}'
+        string += f' The toatal pay is {self.get_pay()}'
+        return string
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
